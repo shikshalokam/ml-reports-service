091f02c17a1912a93fdc136a50dba0afe83c742f
praveenKDass <praveendass@shikshalokam.org>
2024-07-04 20:52:33 +0530

    feat:format the files

--- controllers/v1/core.js
+++ controllers/v1/core.js
@@ -1,18 +1,15 @@
-const pdfHandlerV2 = require('../../helper/common_handler_v2');
+const pdfHandlerV2 = require("../../helper/common_handler_v2");
 
-
-exports.statsReport = async function(req,res){
-    
-    let overview = req.body.data;
-    try{
-        let response = await pdfHandlerV2.coreStatsReportGeneration(overview);
-        res.status(200).send(response);
-    }catch(err){
-        res.status(400).send({
-          result: false,
-          message: 'INTERNAL_SERVER_ERROR'
-        })
-    }
-
- }
- 
+//Function to generate PDF for userOverview stats 
+exports.statsReport = async function (req, res) {
+  let overview = req.body.data;
+  try {
+    let response = await pdfHandlerV2.coreStatsReportGeneration(overview);
+    res.status(200).send(response);
+  } catch (err) {
+    res.status(400).send({
+      result: false,
+      message: "INTERNAL_SERVER_ERROR",
+    });
+  }
+};
